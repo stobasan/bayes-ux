78635b88be86516177b371a1f6ff78ae4c38b06d
Michał Stobiński <stobasan@gmail.com>
2019-06-19 12:11:11 +0200

    Updates

---
+++ precache-manifest.2281403a5b1e43539e5328e638c4ee7b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd40f9638c43bec38c338efe935b6e0f",
+    "url": "/bayes-ux/index.html"
+  },
+  {
+    "revision": "ab8b1d11dba33a5557e4",
+    "url": "/bayes-ux/static/css/main.d44430c8.chunk.css"
+  },
+  {
+    "revision": "a7e598ffbcf104978adf",
+    "url": "/bayes-ux/static/js/2.baba7ba9.chunk.js"
+  },
+  {
+    "revision": "ab8b1d11dba33a5557e4",
+    "url": "/bayes-ux/static/js/main.d054ebd9.chunk.js"
+  },
+  {
+    "revision": "7aff8670c8a1c50a9caa",
+    "url": "/bayes-ux/static/js/runtime~main.5a59bb27.js"
+  },
+  {
+    "revision": "6619a84e4a6717209575502811532fab",
+    "url": "/bayes-ux/static/media/seriesOptimized.6619a84e.jpg"
+  },
+  {
+    "revision": "790e8c12efea06b1a3ab82a500adde3c",
+    "url": "/bayes-ux/static/media/tournamentsOptimized.790e8c12.jpg"
+  }
+]);
