b44362f727ab8d1cf5b2b0ae50269c35b8e19f54
Michał Stobiński <stobasan@gmail.com>
2019-06-19 11:17:01 +0200

    remove ignore from build folder

---
+++ build/precache-manifest.2c93c5a50f693de32e91383a309e172d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f56457b8aa55affdb30d255057fcbbc",
+    "url": "/bayes-ux/index.html"
+  },
+  {
+    "revision": "be7ca4a677aa3231709d",
+    "url": "/bayes-ux/static/css/main.5acfa33c.chunk.css"
+  },
+  {
+    "revision": "a7e598ffbcf104978adf",
+    "url": "/bayes-ux/static/js/2.baba7ba9.chunk.js"
+  },
+  {
+    "revision": "be7ca4a677aa3231709d",
+    "url": "/bayes-ux/static/js/main.d054ebd9.chunk.js"
+  },
+  {
+    "revision": "7aff8670c8a1c50a9caa",
+    "url": "/bayes-ux/static/js/runtime~main.5a59bb27.js"
+  },
+  {
+    "revision": "6619a84e4a6717209575502811532fab",
+    "url": "/bayes-ux/static/media/seriesOptimized.6619a84e.jpg"
+  },
+  {
+    "revision": "790e8c12efea06b1a3ab82a500adde3c",
+    "url": "/bayes-ux/static/media/tournamentsOptimized.790e8c12.jpg"
+  }
+]);
